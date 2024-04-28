f3836312753a8c6d7dff0a41bb862ee07bcad214
starman7475 <sirota_mihail2007@mail.ru>
2024-04-28 12:00:54 +0700

    Update Modal.jsx

--- my-app/src/components/Modal.jsx
+++ my-app/src/components/Modal.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 const modalRoot = document.querySelector("#modal-root");
 
-class Modal extends Component {
-  render() {
-    return ReactDOM.createPortal(
-      <div className="Overlay" onClick={this.props.closeModal}>
-        <div className="Modal">
-          <img src={this.props.fullImage} alt="" />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
+function Modal({ props }) {
+  return ReactDOM.createPortal(
+    <div className="Overlay" onClick={props.closeModal}>
+      <div className="Modal">
+        <img src={props.fullImage} alt="" />
+      </div>
+    </div>,
+    modalRoot
+  );
 }
 
 export default Modal;
